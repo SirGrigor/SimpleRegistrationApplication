7cb2575d405b146a1373a3e8f7abd5452c94e43b
Ilja Grigorjev <ilja.grigorjev@wemakesoftware.eu>
2022-06-06 15:11:34 +0300

    Initial Starter Commit

---
+++ src/main/java/com/registration/entain/SimpleRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.registration.entain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRegistrationApplication.class, args);
+    }
+
+}
